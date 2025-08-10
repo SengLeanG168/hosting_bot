a5670daa95098a9ee0ca9073c93bc1b36629c3ee
SengLeanG168 <hengsengleang7777@gmail.com>
2025-08-10 21:47:14 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-apt-get update
-apt-get install -y tesseract-ocr
-apt-get install -y tesseract-ocr-khm
-
-python khmer_ocr_bot.py
+python [khmer_ocr_bot.py](http://_vscodecontentref_/1)
