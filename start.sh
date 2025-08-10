9e1f1deabef3f4537121f170ec237f40a37318c4
LeanG <healeang222@gmail.com>
2025-08-10 21:23:57 +0700

    Initial commit for Render deployment

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-apt-get update
-apt-get install -y tesseract-ocr
-apt-get install -y tesseract-ocr-khm
+
+# Install tesseract OCR and Khmer language pack
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr
+sudo apt-get install -y tesseract-ocr-khm
+
+# Run the bot
 python khmer_ocr_bot.py
